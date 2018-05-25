007d16c21a0c667d9e0c0b4157d58a7f7004f389
Sean K Smith <SSmith2347@gmail.com>
2018-05-25 16:07:19 -0400

    Added the player prefab and player input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float defaultSpeed;
+	private float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	void FixedUpdate () {
+		HandleMovement();
+	}
+
+	void HandleMovement() {
+		Vector2 moveForce = GetMovementDirection() * GetCurrentPlayerSpeed();
+
+		rb.AddForce(moveForce, ForceMode2D.Force);
+	}
+
+	private Vector2 GetMovementDirection() {
+		return new Vector2(GetPlayerHorizontal(), GetPlayerVertical());
+	}
+
+	private float GetCurrentPlayerSpeed() {
+		return defaultSpeed;
+	}
+
+	private float GetPlayerHorizontal() {
+		float horizontal = Input.GetAxis("Horizontal");
+		return horizontal;
+	}
+
+	private float GetPlayerVertical() {
+		float vertical = Input.GetAxis("Vertical");
+		return vertical;
+	}
+}
