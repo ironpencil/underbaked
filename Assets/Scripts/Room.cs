83de9c6b533bccb70890c014c98cc60f9e357b33
Sean K Smith <SSmith2347@gmail.com>
2018-05-26 09:51:40 -0400

    #12 Added room Connections

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Room : MonoBehaviour {
-	public Door[] doors;
+	public Connection[] connections;
 	
 	// Use this for initialization
 	void Start () {
