b422b54512a816131686873843d984429519c6ca
Sean K Smith <SSmith2347@gmail.com>
2018-05-26 01:15:07 -0400

    #12 Added ship and room framework. Refactored interactable.

--- Assets/Scripts/Interactable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Interactable : MonoBehaviour {
-	public Collider2D triggerZone;
-
-	public abstract void Interact(Character actor);
-}
