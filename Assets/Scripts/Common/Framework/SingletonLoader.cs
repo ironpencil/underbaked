711023000b1a598ecf9527e743a39b63d9dbe829
Jim South <james.a.south@gmail.com>
2018-05-26 01:21:27 -0400

    Basic game state with singletons

---
+++ Assets/Scripts/Common/Framework/SingletonLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonLoader : Singleton<SingletonLoader> {
+
+    public List<GameObject> gameObjects;
+    public bool setChildren = false;
+    public bool destroySelf = true;
+
+	// Use this for initialization
+	public override void Start () {
+        transform.parent = null;
+
+        base.Start();
+        if (this == null) { return; }
+
+        Transform parent = null;
+
+        if (setChildren)
+        {
+            parent = transform;
+            destroySelf = false;
+        }
+
+        foreach (GameObject go in gameObjects)
+        {
+            Instantiate(go, parent);
+        }
+
+        if (destroySelf)
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+}
