fcd03d8322ae278fe5fdd2b167c56c34a98e7b67
Sean K Smith <SSmith2347@gmail.com>
2018-05-25 23:46:06 -0400

    Added player status, death, rebirth, movement state for #10

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -7,12 +7,9 @@ using UnityEngine;
  * 	Class for handling player input
  */
 public class PlayerInput : MonoBehaviour {
-
-	private Rigidbody2D rb;
-	private Player player; // The Rewired Player
-	private int playerId = 0;
-	public float defaultSpeed;
-	private float currentSpeed;
+	private Player player;
+	public Character character;
+	public int playerId = 0;
 
 	void Awake() {
 		player = ReInput.players.GetPlayer(playerId);
@@ -20,56 +17,25 @@ public class PlayerInput : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D>();
+		
 	}
 	
 	void OnUpdate() {
 		
 	}
 
-	void OnTriggerStay2D(Collider2D other) {
-		GameObject otherObject = other.gameObject;
-		if (IsActionButtonPressed() && IsObjectInteractable(otherObject)) {
-			InteractWith(otherObject);
+	void FixedUpdate () {
+		HandleMovement();
+		HandleActionPress();
+	}
+
+	void HandleActionPress() {
+		if (player.GetButtonDown("action")) {
+			character.Interact();
 		}
 	}
 
-	void InteractWith(GameObject otherObject) {
-		otherObject.GetComponent<Interactable>().Interact();
-	}
-
-	bool IsObjectInteractable(GameObject gameObject) {
-		Component c = gameObject.GetComponent<Interactable>();
-		return c != null;
-	}
-
-	void FixedUpdate () {
-		HandleMovement();
-	}
-
 	void HandleMovement() {
-		Vector2 moveForce = GetMovementDirection() * GetCurrentPlayerSpeed();
-
-		rb.AddForce(moveForce, ForceMode2D.Force);
-	}
-
-	private bool IsActionButtonPressed() {
-		return player.GetButtonDown("action");
-	}
-
-	private Vector2 GetMovementDirection() {
-		return new Vector2(GetPlayerHorizontal(), GetPlayerVertical());
-	}
-
-	private float GetCurrentPlayerSpeed() {
-		return defaultSpeed;
-	}
-
-	private float GetPlayerHorizontal() {
-		return player.GetAxis("moveHorizontal");;
-	}
-
-	private float GetPlayerVertical() {
-		return player.GetAxis("moveVertical");;
+		character.Move(new Vector2(player.GetAxis("moveHorizontal"), player.GetAxis("moveVertical")));
 	}
 }
