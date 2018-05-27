b5794ab6b707fa3d56b4ffb2d82618b742b453be
Sean K Smith <SSmith2347@gmail.com>
2018-05-27 11:03:41 -0400

    Does this sheep seem a little leaky to you? #22

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Room : MonoBehaviour {
 	public Connection[] connections;
-	
+	public List<Transform> availLeakLocations;
+	public List<GameObject> leaks;
+	public enum State {
+		EMPTY, FLOODED
+	}
+	public State state;
+	public GameObject flood;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,5 +20,14 @@ public class Room : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public bool IsFlooding() {
+		return state == State.FLOODED;
+	}
+
+	public void Flood() {
+		state = State.FLOODED;
+		flood.SetActive(true);
 	}
 }
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour {
+	public List<Connection> connections;
+	public List<Room> rooms;
+	public float leakFreqMin;
+	public float leakFreqMax;
+	private float nextLeakTime;
+	public GameObject leakPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextLeakTime) {
+            nextLeakTime = Time.time + Random.Range(leakFreqMin, leakFreqMax);
+			SpringRandomLeak();
+        }
+
+		SpreadWater();
+	}
+
+	void SpreadWater() {
+		foreach (Connection conn in connections) {
+			if ((conn.roomA.IsFlooding() || conn.roomB.IsFlooding()) 
+			&& conn.door.IsOpen()) {
+				conn.roomA.Flood();
+				conn.roomB.Flood();
+			}
+		}
+	}
+
+	private void SpringRandomLeak() {
+		Room room = GetRandomRoom();
+		AddRandomLeak(room);
+	}
+
+	private Room GetRandomRoom() {
+		int roomIdx = Random.Range(0, rooms.Count);
+		return rooms[roomIdx];
+	}
+
+	private void AddRandomLeak(Room room) {
+		if (room.availLeakLocations.Count > 0) {
+			int leakLocIdx = Random.Range(0, room.availLeakLocations.Count);
+			Transform location = room.availLeakLocations[leakLocIdx];
+			room.availLeakLocations.RemoveAt(leakLocIdx);
+			room.leaks.Add(Instantiate(leakPrefab, location));
+			room.Flood();
+		}
+	}
+}
